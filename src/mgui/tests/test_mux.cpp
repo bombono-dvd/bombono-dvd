49fa2f433d4387e3e21117bcf68e8a38600c9975
Ilya Murav'jov <muravev@yandex.ru>
2010-04-10 17:15:26 +0400

    test for bug with GtkFileChooser, http://bugzilla.gnome.org/show_bug.cgi?id=615353

--- src/mgui/tests/test_mux.cpp
+++ src/mgui/tests/test_mux.cpp
@@ -6,6 +6,28 @@
 
 #include <mgui/win_utils.h>
 
+#include <mgui/sdk/widget.h>
+#include <mgui/sdk/window.h>
+#include <mgui/sdk/packing.h>
+
+BOOST_AUTO_TEST_CASE( TestFileChooser )
+{
+    InitGtkmm();
+
+    const char* path = "/var/tmp";
+    Gtk::FileChooserButton btn("Select folder", Gtk::FILE_CHOOSER_ACTION_SELECT_FOLDER);
+
+    BOOST_CHECK( btn.set_filename(path) );
+    //BOOST_CHECK( btn.set_current_folder(path) );
+
+    //Gtk::Window win;
+    //win.add(btn);
+    //RunWindow(win);
+    IteratePendingEvents();
+    
+    //io::cout << btn.get_filename() << io::endl;
+    BOOST_CHECK_MESSAGE( strcmp(btn.get_filename().c_str(), path) == 0, "See more at https://bugzilla.gnome.org/show_bug.cgi?id=615353" );
+}
 
 BOOST_AUTO_TEST_CASE( TestPreferences )
 {
