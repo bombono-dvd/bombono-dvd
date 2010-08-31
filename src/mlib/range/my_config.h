13c7d4c006f3b4c7a785df558c74d490ffaf116d
Ilya Murav'jov <muravev@yandex.ru>
2010-09-01 01:30:07 +0400

    warnings fix

--- src/mlib/range/my_config.h
+++ src/mlib/range/my_config.h
@@ -2,13 +2,8 @@
 #define __MLIB_RANGE_MY_CONFIG_H__
 
 // отключаем ради старых версий буста
-#define BOOST_RANGE_ENABLE_CONCEPT_ASSERT 0
-
-#if BOOST_RANGE_ENABLE_CONCEPT_ASSERT
-    #define BOOST_RANGE_CONCEPT_ASSERT( x ) BOOST_CONCEPT_ASSERT( x )
-#else
-    #define BOOST_RANGE_CONCEPT_ASSERT( x )
-#endif
+#undef BOOST_RANGE_CONCEPT_ASSERT
+#define BOOST_RANGE_CONCEPT_ASSERT( x )
 
 #endif // #ifndef __MLIB_RANGE_MY_CONFIG_H__
 
